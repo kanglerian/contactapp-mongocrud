405c5e3f8f66ca8310b3025991dd49d415ee9758
kampusinformatika <kanglerian.business@gmail.com>
2022-05-12 16:33:19 +0700

    Revert "spreadsheet"
    This reverts commit a7770b85fe54ef49c7cfa6f2a276de01c23c9700.

--- utils/db.js
+++ utils/db.js
@@ -1,8 +1,17 @@
-var mysql = require('mysql');
-
-var db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "contactapp"
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/kanglerian',
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
 });
+
+/* Menambah 1 data
+const contact1 = new Contact({
+    nama: 'Naima',
+    nohp: '08122233444',
+    email: 'naima@gmail.com'
+}); */
+
+/* Simpan ke collections
+contact1.save().then((result) => console.log(result)).catch((error) => console.log(error)); */
